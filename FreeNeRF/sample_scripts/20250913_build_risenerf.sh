eaed4ddc3764c40d07cc47e46f9f266e57d4bed8
mickey0226 <kojimaofficial88@gmail.com>
2025-09-13 07:48:16 +0000

    confirmed that risenerf dir was succesfully created

---
+++ FreeNeRF/sample_scripts/20250913_build_risenerf.sh
@@ -0,0 +1,27 @@
+
+cd /root/FreeNeRF
+
+method=risenerf
+
+num_shots=6
+project=llff${num_shots}-$method
+scans=(fern flower fortress horns leaves orchids room trex)
+for scan in "${scans[@]}" # 配列の全要素をループ
+do
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+    python3 train.py \
+        --gin_configs configs/$method/llff${num_shots}_$method.gin \
+        --gin_bindings "Config.llff_scan = '$scan'" \
+        --gin_bindings "Config.expname = '$scan-train'" \
+        --gin_bindings "Config.checkpoint_dir = 'out/$method/$project/$scan'" \
+        --gin_bindings "Config.project = '$project'" \
+        --gin_bindings "Config.render_chunk_size = 16384" 
+    exit
+    python3 eval.py \
+        --gin_configs configs/$method/llff${num_shots}_$method.gin \
+        --gin_bindings "Config.llff_scan = '$scan'" \
+        --gin_bindings "Config.expname = '$scan-eval'" \
+        --gin_bindings "Config.checkpoint_dir = 'out/$method/$project/$scan'" \
+        --gin_bindings "Config.log_img_to_wandb = True" \
+        --gin_bindings "Config.project = '$project'"
+done
